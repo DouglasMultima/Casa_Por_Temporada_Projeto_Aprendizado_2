94b174417c753b894fb83ed616fb542cf3f88d35
Douglas <douglas.multima@hotmail.com>
2023-11-10 12:40:21 -0400

    Subindo projeto no repositório

---
+++ app/src/main/java/com/example/casaportemporada/activity/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.casaportemporada.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.casaportemporada.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+        
+
+    }
+}
